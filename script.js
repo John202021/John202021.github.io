ea3025074071820531dbdb241c5af1064dbc0613
John202021 <johnyt2040@gmail.com>
2022-09-06 16:46:31 +0300

    styling the toggle button in change

--- script.js
+++ script.js
@@ -9,13 +9,20 @@ toggleButton.addEventListener("click", async () => {
     navbarLinks.classList.toggle("active");
 
     if (navbarLinks.classList.contains("active")) {
+        toggleButton.style.transform = "rotate(90deg)";
         for (let height = 0; height <= max_height; height += 0.25) {
             console.log(height + "%");
     
             navbarLinks.style.height = height + "rem"
+
+            if (!navbarLinks.classList.contains("active")) {
+                navbarLinks.style.height = 0;
+                break;
+            }
             await wait(7);
         } 
     } else {
+        toggleButton.style.transform = "rotate(0deg)"
         navbarLinks.style.height = 0;
     }
     

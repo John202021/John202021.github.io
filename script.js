ea82707aed5acb199244260fdd8c8349136032a9
John202021 <johnyt2040@gmail.com>
2022-09-06 19:51:06 +0300

    fixing navbar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navLinksLength = document.getElementsByClassName("nav-link").length;
+const mediaQuery = window.matchMedia("(min-width: 715px)");
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 const max_height = navLinksLength * 2.25;
@@ -10,21 +11,30 @@ toggleButton.addEventListener("click", async () => {
 
     if (navbarLinks.classList.contains("active")) {
         toggleButton.style.transform = "rotate(90deg)";
+        let msWait = 0;
         for (let height = 0; height <= max_height; height += 0.25) {
-            console.log(height + "%");
-    
+            
             navbarLinks.style.height = height + "rem"
 
             if (!navbarLinks.classList.contains("active")) {
                 navbarLinks.style.height = 0;
                 break;
             }
-            await wait(7);
+            await wait(msWait);
+            msWait += 0.25;
         } 
     } else {
         toggleButton.style.transform = "rotate(0deg)"
         navbarLinks.style.height = 0;
     }
-    
-
 });
+
+mediaQuery.addEventListener("change", handleMediaChange);
+
+
+function handleMediaChange(media) {
+    if (media.matches) {
+        navbarLinks.style.height = "unset";
+    }
+}
+

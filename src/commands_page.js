45bf0843a6c1011c924d4a7f0a36de0de3f79e18
John202021 <johnyt2040@gmail.com>
2022-09-15 22:06:54 +0300

    making responsible the categories list

--- src/commands_page.js
+++ src/commands_page.js
@@ -1,28 +1,46 @@
 import { commands_list } from "./commands_list.js";
 
 const mediaQuery = window.matchMedia("(max-width: 900px)");
-const commands_dom = document.getElementById("commands").tBodies[0];
+let commands_dom = document.getElementById("commands").querySelector("tbody");
 const categories_dom = document.getElementById("categories").querySelector("ul");
-const formatted_commands_names = commands_list.map(c => c.name);
-const formatted_commands_descriptions = commands_list.map(c => c.description);
-const formatted_commands_usages = commands_list.map(c => "=" + c.usage);
+let lastLiElem = document.getElementById("all-choice");
 
-let commands_categories_obj = {
-    ADMIN: [],
-    CURRENCY: [],
-    FUN: [],
-    INFORMATION: [],
-    LEVELING: [],
-    MISC: [],
-    NORMAL: []
-};
+let current_category;
+let categories_list = [];
+commands_list.forEach(c => {
+    if (!categories_list.includes(c.category)) return categories_list.push(c.category);
+    return;
+})
 
-let table_row1_array = [];
-let table_row2_array = [];
-let table_row3_array = [];
+let table_row1_obj = {};
+let table_row2_obj = {};
+let table_row3_obj = {};
+
+let commands_categories_obj = {};
+categories_list.forEach(category => {
+    commands_categories_obj[category] = [];
+    table_row1_obj[category] = [];
+    table_row2_obj[category] = [];
+    table_row3_obj[category] = [];
+});
 
 commands_list.forEach(command => {
     commands_categories_obj[command.category].push(command);
+
+    let td = document.createElement("td");
+    td.innerHTML = command.name;
+    td.setAttribute("class", "name-col");
+    table_row1_obj[command.category].push(td);
+
+    let td2 = document.createElement("td");
+    td2.innerHTML = command.description;
+    td2.setAttribute("class", "descr-col");
+    table_row2_obj[command.category].push(td2);
+
+    let td3 = document.createElement("td");
+    td3.innerHTML = "=" + command.usage;
+    td3.setAttribute("class", "usage-col");
+    table_row3_obj[command.category].push(td3);
 })
 
 Object.keys(commands_categories_obj).forEach(category => {
@@ -34,42 +52,43 @@ Object.keys(commands_categories_obj).forEach(category => {
 
     li.innerHTML = formatted_categ;
     li.setAttribute("class", "category");
-    li.setAttribute("tabindex", "1");
     categories_dom.appendChild(li);
 })
 
+console.log(categories_dom);
+categories_dom.addEventListener("click", handleCategoryClick);
 
-formatted_commands_names.forEach((name) => {
-    let td = document.createElement("td");
-    td.innerHTML = name;
-    td.setAttribute("class", "name-col");
-    table_row1_array.push(td);
-});
+function formatTableData(categoryChoice, bool = false) {
+    mediaQuery.addEventListener("change", toggleTableRows);
 
-formatted_commands_descriptions.forEach((descr) => {
-    let td = document.createElement("td");
-    td.innerHTML = descr;
-    td.setAttribute("class", "descr-col");
-    table_row2_array.push(td);
-});
+    while(commands_dom.rows.length > 0) {
+        commands_dom.deleteRow(0);
+      }
 
-formatted_commands_usages.forEach((usage) => {
-    let td = document.createElement("td");
-    td.innerHTML = usage;
-    td.setAttribute("class", "usage-col");
-    table_row3_array.push(td);
-});
-
-function formatTableData(bool = false) {
-    let rowCount = commands_dom.rows.length;
-    for (var i = 1; i < rowCount; i++) {
-        commands_dom.deleteRow(1);
+    if ((categoryChoice || current_category) && categoryChoice !== "All") {
+        const category = categoryChoice?.toLocaleUpperCase() || current_category;
+        console.log("Category " + category);
+            outputCommandTable(category, bool);
+        current_category = category;
+        console.log("Current category " + current_category);
+        return;
     }
+    Object.keys(commands_categories_obj).forEach(category => {
+        outputCommandTable(category, bool);
+    })
+}
+formatTableData();
 
-    for (let index = 0; index < commands_list.length; index++) {
-        const name_data = table_row1_array[index];
-        const descr_data = table_row2_array[index];
-        const usage_data = table_row3_array[index];
+function toggleTableRows(event) {
+    formatTableData("", event.matches);
+}
+
+function outputCommandTable(category, bool) {
+
+    for (let index = 0; index < commands_categories_obj[category].length; index++) {
+        const name_data = table_row1_obj[category][index];
+        const descr_data = table_row2_obj[category][index];
+        const usage_data = table_row3_obj[category][index];
 
         let tableRow = document.createElement("tr");
         if (mediaQuery.matches || bool) {
@@ -91,12 +110,16 @@ function formatTableData(bool = false) {
             commands_dom.appendChild(tableRow);
         }
     }
-    commands_dom.style.border = "2px solid rgb(140, 138, 138)"
-    mediaQuery.addEventListener("change", toggleTableRows);
+
 }
 
-formatTableData();
+function handleCategoryClick(event) {
+    if (event.target.tagName === "LI") {
+        event.target.style.background = "rgb(160, 27, 69)";
+        if (lastLiElem && lastLiElem.innerText !== event.target.innerText) lastLiElem.style.background = "transparent";
 
-function toggleTableRows(event) {
-    formatTableData(event.matches);
+        formatTableData(event.target.innerText);
+        lastLiElem = event.target;
+    }
+    return;
 }

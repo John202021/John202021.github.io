af8ee42bdebf4050b64613b38560c99c4e0fe0f5
John202021 <johnyt2040@gmail.com>
2022-09-15 14:25:12 +0300

    adding categories list

--- src/commands_page.js
+++ src/commands_page.js
@@ -2,33 +2,61 @@ import { commands_list } from "./commands_list.js";
 
 const mediaQuery = window.matchMedia("(max-width: 715px)");
 const commands_dom = document.getElementById("commands").tBodies[0];
+const categories_dom = document.getElementById("categories").querySelector("ul");
 const formatted_commands_names = commands_list.map(c => c.name);
 const formatted_commands_descriptions = commands_list.map(c => c.description);
 const formatted_commands_usages = commands_list.map(c => "=" + c.usage);
-console.log(commands_dom);
+
+let commands_categories_obj = {
+    ADMIN: [],
+    CURRENCY: [],
+    FUN: [],
+    INFORMATION: [],
+    LEVELING: [],
+    MISC: [],
+    NORMAL: []
+};
 
 let table_row1_array = [];
 let table_row2_array = [];
 let table_row3_array = [];
 
+commands_list.forEach(command => {
+    commands_categories_obj[command.category].push(command);
+})
+
+Object.keys(commands_categories_obj).forEach(category => {
+    let li = document.createElement("li");
+
+    let lowerCaseCat = category.toLocaleLowerCase();
+    let firstLetter = lowerCaseCat.charAt(0).toLocaleUpperCase();
+    let formatted_categ = firstLetter + lowerCaseCat.slice(1);
+
+    li.innerHTML = formatted_categ;
+    li.setAttribute("class", "category");
+    li.setAttribute("tabindex", "1");
+    categories_dom.appendChild(li);
+})
+
+
 formatted_commands_names.forEach((name) => {
     let td = document.createElement("td");
     td.innerHTML = name;
-    td.setAttribute("class", "name-row");
+    td.setAttribute("class", "name-col");
     table_row1_array.push(td);
 });
 
 formatted_commands_descriptions.forEach((descr) => {
     let td = document.createElement("td");
     td.innerHTML = descr;
-    td.setAttribute("class", "descr-row");
+    td.setAttribute("class", "descr-col");
     table_row2_array.push(td);
 });
 
 formatted_commands_usages.forEach((usage) => {
     let td = document.createElement("td");
     td.innerHTML = usage;
-    td.setAttribute("class", "usage-row");
+    td.setAttribute("class", "usage-col");
     table_row3_array.push(td);
 });
 
